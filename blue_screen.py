073b83ed8ae04df61f74a663454515cb8af364ee
WnndGws <WnndGws@users.noreply.github.com>
2021-08-20 23:40:22 +0800

    Add outline for script

---
+++ blue_screen.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Check if youtube video is a blue screen.
+
+A blue screen indicates that my HDMI cable has come loose
+
+Step 1) Get temp screenshot of youtube url as flag variable
+Step 2) Check colour of pixel in middle of video and match with blue
+Step 3) Alert user somehow
+"""
+
+# standard imports
+# 3rd party imports
